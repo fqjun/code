ee30ee22664185208f9ac086a9d82bcab65e9886
fqjun <1767851382@qq.com>
2021-01-07 21:06:45 +0800

    update data

--- xml_test/main.cpp
+++ xml_test/main.cpp
@@ -6,60 +6,83 @@ using namespace std;
 
 int main()
 {
-//    ofstream file ("test.csv");
-//    file<<format(mat,Formatter::FMT_CSV);
-//    file.close();
-
-
-
-    vector<vector<int> >vec;
-    vector<int>value;
-    vector<char>line;
-
-    value.push_back(123);
-    value.push_back(2);
-
-//    Mat mat = Mat::ones(Size(2,value.size()),CV_8UC1);
+    //声明传值和整合Mat以及vector
     Mat mat ;
-    mat.push_back(static_cast<Mat>(value));
 
-    cout<<mat.size<<endl;
+    vector<float>test_float;
+    vector<float>test;
+//    vector<int>test1;
 
-    for(auto i = 0;i<mat.rows;++i){
+    Mat value;
+    Mat line;
+    Mat mat_value/* = Mat(3,1,CV_32F)*/;
 
-        uchar* data = mat.ptr<uchar>(i);
+//    cout<<"mat_value.size = "<<mat_value.size<<endl;
+    //对mat和vector赋值
+    test.push_back(2352.34);
+    test.push_back(1);
+    test.push_back(8882.34);
 
-        for(auto j = 0;j<mat.cols;++j){
+    test_float.push_back(4899.345345);
+    test_float.push_back(13.231);
+    test_float.push_back(5733.231);
 
-            cout<<"data["<<i<<"]["<<j<<"]="<<(int)data[j];
+    cout<<"test = "<<test.at(0)<<endl;
+    cout<<"test = "<<test.at(1)<<endl;
+    cout<<"test = "<<test.at(2)<<endl;
+
+    cout<<"test_float = "<<test_float.at(0)<<endl;
+    cout<<"test_float = "<<test_float.at(1)<<endl;
+    cout<<"test_float = "<<test_float.at(2)<<endl;
+
+    cout<<"test.size() = "<<test.size()<<endl;
+
+    auto iter_test = test.begin();
+    auto iter_test_float = test_float.begin();
+
+    mat_value = Mat(test.size(),2,CV_32F);
+    cout<<"mat_value.rows = "<<mat_value.rows<<endl;
+    cout<<"mat_value.cols = "<<mat_value.cols<<endl;
+
+    for(auto i = 0;i<mat_value.rows;++i){
+
+        float* data = mat_value.ptr<float>(i);
+
+        for(auto j = 0;j<mat_value.cols;++j){
+
+            data[0] = *(iter_test + i);
+            data[1] = *(iter_test_float + i);
+
+            cout<<"data["<<i<<"]["<<j<<"]="<<(float)data[j]<<"  ";
+
+//              cout<<"data["<<i<<"]["<<j<<"]="<<mat_value.at<float>(i,j)<<"  ";
+
 
         }
         cout<<endl;
     }
 
-//        for (int i=1;i<5;++i)
-//        {
-//            int b = 0;
-//            cout<<"b="<<endl;
-//            cin>>b;
 
-//            for (int j=0;j<b;++j)
-//            {
-//                int a;
-//                cout<<"a="<<endl;
-//                cin>>a;
-//                array1.push_back(a);
-//            }
-//            vec.push_back(array1);
-//            array1.clear();
 
-//        }
+    //打开csv文件并传入数据
+    ofstream file ("test.csv");
+    file<<format(mat_value,Formatter::FMT_CSV);
+    file.close();
 
-//        for(auto i = 0;i<vec.size();++i){
-//            for(auto j =0;j<vec[i].size();++j){
-//                cout<<"vec["<<i<<"]["<<j<<"] value = "<<vec[i][j]<<endl;
-//            }
-//            cout<<endl;
-//        }
+    //打印mat里面的数据
+    cout<<mat.size<<endl;
+
+    for(auto i = 0;i<mat_value.rows;++i){
+
+        float* data = mat_value.ptr<float>(i);
+
+        for(auto j = 0;j<mat_value.cols;++j){
+
+            cout<<"data["<<i<<"]["<<j<<"]="<<(float)data[j]<<"  ";
+
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
